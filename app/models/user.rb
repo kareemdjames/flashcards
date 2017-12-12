b760431b298a7f26479a05ad0e6274d320beaadf
Kareem James <kareemdjames@gmail.com>
2017-12-11 20:39:34 -0500

    created flashcard model, controller and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # when we delete a user, all the flashcards linked to this user are being deleted as well
+  has_many :flashcards, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212004754) do
+ActiveRecord::Schema.define(version: 20171212013214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flashcards", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "question"
+    t.text "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flashcards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20171212004754) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "flashcards", "users"
 end
