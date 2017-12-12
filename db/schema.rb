eb2c95403a270d42adcd054a04c45ac76eff6c1c
Kareem James <kareemdjames@gmail.com>
2017-12-11 21:30:00 -0500

    created tagging model(join table)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212020817) do
+ActiveRecord::Schema.define(version: 20171212021823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20171212020817) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_flashcards_on_user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "flashcard_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flashcard_id"], name: "index_taggings_on_flashcard_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -40,5 +49,7 @@ ActiveRecord::Schema.define(version: 20171212020817) do
   end
 
   add_foreign_key "flashcards", "users"
+  add_foreign_key "taggings", "flashcards"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "tags", "users"
 end
